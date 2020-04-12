d15287bbe1202d8bfe04255e3a8f1644ac3f6f76
karlwitek <karl.witek20@gmail.com>
2020-04-11 17:46:01 -0700

    add product list to local storage

--- products/products.js
+++ products/products.js
@@ -1,14 +1,28 @@
 import campEquipment from '../data/camp-equip.js';
 import renderCampEquip from './render-function.js';
 
-// for loop
+const PRODUCTS_KEY = 'PRODUCTS';
+let equipmentList = localStorage.getItem(PRODUCTS_KEY);
 
+// if there are no campEquipment , seed the data
+if (!equipmentList) {
+    // seed data
+    equipmentList = JSON.stringify(campEquipment);
+
+    localStorage.setItem(PRODUCTS_KEY, equipmentList);
+
+    //equipmentList = localStorage.getItem(PRODUCTS_KEY);
+    // still a string , parse  below and use in for loop
+
+}
 const list = document.getElementById('list');
 
-for (let i = 0; i < campEquipment.length; i ++) {
-    const item = campEquipment[i];
+const parsedEquipmentList = JSON.parse(equipmentList);
 
-    const elem = renderCampEquip(item);
-    list.appendChild(elem);
+for (let i = 0; i < parsedEquipmentList.length; i ++) {
+    const item = parsedEquipmentList[i];
+
+    const itemDisplay = renderCampEquip(item);
+    list.appendChild(itemDisplay);
 }
 
